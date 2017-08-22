a5b01b0b4c12ac1019a03d7cb11c7bd072e23a10
Anonymous <deepak.tiwari@aricent.com>
2017-08-22 15:24:31 +0530

    changes for VNFs

--- labs/osbash/scripts/ubuntu/start_tacker_server.sh
+++ labs/osbash/scripts/ubuntu/start_tacker_server.sh
@@ -15,9 +15,11 @@ exec_logfile
 
 indicate_current_auto
 # tacker is to be started manually after setup is up. Tacker start in automation do not exit from script. 
+#run_process "$1" "$2" >$SERVICE_DIR/$SCREEN_NAME/$1.pid
+
 sudo python /usr/local/bin/tacker-server \
     --config-file /usr/local/etc/tacker/tacker.conf \
-    --log-file /var/log/tacker/tacker.log &
+    --log-file /var/log/tacker/tacker.log > "$HOME/tacker-server-console.log" &
 
 source admin-openrc.sh
 tacker vim-register --is-default --config-file /etc/tacker/config.yaml --description Aricent_Gurugram_network VIM.Aricent.Gurugram
